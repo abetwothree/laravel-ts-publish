a53283d5db330b9860d7c75568e1ba0f4f325d9d
Abraham Arango <skat_et_dieu@yahoo.com>
2026-05-18 18:08:35 -0500

    Update stubs

--- workbench/resources/js/types/data/full-template-example/app/http/resources/post-resource.ts
+++ workbench/resources/js/types/data/full-template-example/app/http/resources/post-resource.ts
@@ -2,6 +2,7 @@ import { type AsEnum } from '@tolki/ts';
 
 import { Priority, Status, Visibility } from '../../enums';
 import type { User } from '../../models';
+import type { CommentResource } from '.';
 
 /**
  * @see Workbench\App\Http\Resources\PostResource
@@ -19,4 +20,19 @@ export interface PostResource
     priority: AsEnum<typeof Priority> | null;
     priority_new: AsEnum<typeof Priority> | null;
     comments: { id: number; content: string; user: User }[];
+    published: boolean;
+    rating_display: number;
+    word_count: string;
+    heading_content: unknown[];
+    publishable: boolean;
+    comments_count: number;
+    is_featured: boolean;
+    category_is_first?: boolean | null;
+    category_is_active?: boolean | null;
+    category_breadcrumb?: string | null;
+    comments_resolved?: CommentResource[];
+    post_class_name: string;
+    post_table_name: string;
+    category_class_name?: string;
+    category_table_name?: string;
 }
