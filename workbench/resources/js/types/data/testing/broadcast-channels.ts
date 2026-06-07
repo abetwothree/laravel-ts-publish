e17d9fb692730dca60c86d7456a55e4fc34f54a7
Abraham Arango <skat_et_dieu@yahoo.com>
2026-06-07 12:07:50 -0500

    Update testing and TS output

--- workbench/resources/js/types/data/testing/broadcast-channels.ts
+++ workbench/resources/js/types/data/testing/broadcast-channels.ts
@@ -2,7 +2,13 @@ export type BroadcastChannel =
     | `orders.${string | number}`
     | `user.${string | number}.notifications`
     | `chat.${string | number}.messages`
-    | `public-announcements`;
+    | `public-announcements`
+    | `order.${string | number}`
+    | `post.${string | number}.comment.${string | number}`
+    | `order-status.${string | number}`
+    | `color-theme.${string | number}`
+    | `role-dashboard.${string | number}`
+    | `teams.${string | number}.rooms.${string | number}`;
 
 export const BroadcastChannels = {
     orders: (orderId: string | number) => `orders.${orderId}` as const,
@@ -13,4 +19,14 @@ export const BroadcastChannels = {
         messages: `chat.${roomId}.messages` as const,
     }),
     "public-announcements": `public-announcements` as const,
+    order: (orderId: string | number) => `order.${orderId}` as const,
+    post: (postId: string | number) => ({
+        comment: (commentId: string | number) => `post.${postId}.comment.${commentId}` as const,
+    }),
+    "order-status": (statusId: string | number) => `order-status.${statusId}` as const,
+    "color-theme": (colorId: string | number) => `color-theme.${colorId}` as const,
+    "role-dashboard": (roleId: string | number) => `role-dashboard.${roleId}` as const,
+    teams: (teamId: string | number) => ({
+        rooms: (roomName: string | number) => `teams.${teamId}.rooms.${roomName}` as const,
+    }),
 };
