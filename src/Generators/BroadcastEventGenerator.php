fda005932fb3531cbf5593c0f6b412fa65ce039f
Abraham Arango <skat_et_dieu@yahoo.com>
2026-06-07 17:50:18 -0500

    feat: add BroadcastEventGenerator

---
+++ src/Generators/BroadcastEventGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AbeTwoThree\LaravelTsPublish\Generators;
+
+use AbeTwoThree\LaravelTsPublish\Transformers\BroadcastEventTransformer;
+use AbeTwoThree\LaravelTsPublish\Writers\CoreWriter;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Override;
+
+/**
+ * @extends CoreGenerator<ShouldBroadcast>
+ */
+class BroadcastEventGenerator extends CoreGenerator
+{
+    public protected(set) BroadcastEventTransformer $transformer;
+
+    #[Override]
+    public function generate(): string
+    {
+        /** @var BroadcastEventTransformer $transformer */
+        $transformer = resolve(config()->string('ts-publish.broadcast_events.transformer_class'), [
+            'findable' => $this->findable,
+        ]);
+        $this->transformer = $transformer;
+
+        /** @var CoreWriter<BroadcastEventTransformer> $writer */
+        $writer = resolve(config()->string('ts-publish.broadcast_events.writer_class'));
+
+        return $this->content = $writer->write($this->transformer);
+    }
+
+    #[Override]
+    public function filename(): string
+    {
+        return $this->transformer->filename();
+    }
+}
