395da66b283d64b771c43b3f4a48beb275667f09
Abraham Arango <skat_et_dieu@yahoo.com>
2026-06-26 08:29:14 -0500

    Update ts-publish.php

--- workbench/config/ts-publish.php
+++ workbench/config/ts-publish.php
@@ -365,11 +365,7 @@ return [
         'inertia_middleware_path' => null,
         'augmentation_filename' => 'inertia-config.d.ts',
         'output_directory' => '',
-
-        // Inertia UI Table page props import `TableResource` from this package.
-        // When null, auto-detects @inertiaui/table-vue or @inertiaui/table-react
-        // from package.json, defaulting to @inertiaui/table-vue.
-        'table_package' => null,
+        'ui_table_package' => null,
     ],
 
     /*
